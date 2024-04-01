409e2f0baa65fa56262fe2cd9e76ba66b7b3e8fa
Камиль Мусин <musin.kamil-2002@list.ru>
2024-04-01 14:40:26 +0300

    Вроде все починил надеюсь

--- the_snake.py
+++ the_snake.py
@@ -133,14 +133,14 @@ class Snake(GameObject):
 
     def draw(self):
         """Отрисовывает змейку на экране."""
-        for position in self.positions[:-1]:
-            rect = (pygame.Rect(position, (GRID_SIZE, GRID_SIZE)))
-            pygame.draw.rect(screen, self.body_color, rect)
-            pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
-
         head_rect = pygame.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(screen, self.body_color, head_rect)
         pygame.draw.rect(screen, BORDER_COLOR, head_rect, 1)
+
+        for position in self.positions[1:]:
+            body_rect = pygame.Rect(position, (GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(screen, self.body_color, body_rect)
+            pygame.draw.rect(screen, BORDER_COLOR, body_rect, 1)
 
         if self.last:
             last_rect = pygame.Rect(self.last, (GRID_SIZE, GRID_SIZE))
